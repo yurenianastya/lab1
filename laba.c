dd73bc984feae7c022683b03514cdeffc7f8e9d5
yurenianastya <43321431+yurenianastya@users.noreply.github.com>
2018-09-17 22:59:49 +0300

    Add files via upload

---
+++ laba.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+void main(void);
+{
+    double x,y;
+    int z = pow(x,cos(y)) - pow(tan(x),sqrt(y-x)) - 19.12*log(pow(y,3));
+    printf(z);
+    return 0;
+}
